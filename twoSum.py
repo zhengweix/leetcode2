5a0a8b54f331e1cf8c8a7bf1f7cafb1313d29555
zhengweix <zheng.wei@famewell.com>
2022-02-17 00:01:57 -0800

    Added 220217

--- twoSum.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for i, v in enumerate(nums):
-            if v in d:
-                return d[v], i
-
-            d[target - v] = i
