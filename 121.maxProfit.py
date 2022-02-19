4ab41bbc7bf42a8d70f45181eef39d0badba8daf
zhengweix <zheng.wei@famewell.com>
2022-02-19 01:59:47 -0800

    Added 220218

--- 121.maxProfit.py
+++ 121.maxProfit.py
@@ -1,17 +1,8 @@
 class Solution:
     def maxProfit(self, prices: list) -> int:
-        n = len(prices)
         minPrice = prices[0]
         profit = 0
-        for i in range(n-1):
-            if prices[i] >= prices[i+1]:
-                minPrice = min(minPrice, prices[i+1])
-            else:
-                if profit < prices[i+1] - minPrice:
-                    profit = prices[i+1] - minPrice
+        for price in prices:
+            minPrice = min(minPrice, price)
+            profit = max(profit, price - minPrice)
         return 0 if profit < 0 else profit
-
-
-
-S = Solution()
-print(S.maxProfit([7,1,5,3,6,4]))
