be55768a6dde3373a371d29e9882736c61fd6fbb
zhengweix <zheng.wei@famewell.com>
2022-03-04 14:16:18 -0800

    Added 220304

--- 35.searchInsert.py
+++ 35.searchInsert.py
@@ -1,2 +1,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            mid = nums[m]
+            if mid == target:
+                return m
+            elif mid > target:
+                r = m - 1
+            else:
+                l = m + 1
+
+        return l
