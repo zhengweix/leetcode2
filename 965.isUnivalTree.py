6f4cbdb2e335efa90fef6372b6c3539e6083ba07
zhengweix <zheng.wei@famewell.com>
2022-03-05 18:58:54 -0800

    Added 220305

--- 965.isUnivalTree.py
+++ 965.isUnivalTree.py
@@ -6,4 +6,14 @@
 #         self.right = right
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        if root.left and root.right:
+            return self.isUnivalTree(root.left) and self.isUnivalTree(root.right) and root.val == root.left.val and root.val == root.right.val
+
+        elif root.left:
+            return self.isUnivalTree(root.left) and root.val == root.left.val
+
+        elif root.right:
+            return self.isUnivalTree(root.right) and root.val == root.right.val
+
+        return True
 
