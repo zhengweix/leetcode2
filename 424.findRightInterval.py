74a8ec080c68c8f438ffeee1caaab2a8d7fd3d43
zhengweix <zheng.wei@famewell.com>
2022-04-30 00:20:12 -0700

    Added 220430

--- 424.findRightInterval.py
+++ 424.findRightInterval.py
@@ -1,2 +1,13 @@
+from collections import defaultdict
 class Solution:
-    def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+    def characterReplacement(self, s: str, k: int) -> int:
+        start, l, m = 0, 0, 0
+        ch = defaultdict(int)
+        for end, c in enumerate(s):
+            ch[c] += 1
+            m = max(m, ch[c])
+            if end - start + 1 - m > k:
+                ch[s[start]] -= 1
+                start += 1
+
+        return end - start + 1
