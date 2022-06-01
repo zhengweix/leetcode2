c0777e0865458933449fc1581407745bf7c64d60
zhengweix <zheng.wei@famewell.com>
2022-06-01 02:39:34 -0700

    Added 220601

--- 1539.findKthPositive.py
+++ 1539.findKthPositive.py
@@ -1,2 +1,28 @@
 class Solution:
+    '''
+    Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+    Return the kth positive integer that is missing from this array.
+
+    Input: arr = [2,3,4,7,11], k = 5
+    Output: 9
+    Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+
+    Input: arr = [1,2,3,4], k = 2
+    Output: 6
+    Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+
+    Constraints:
+    1 <= arr.length <= 1000
+    1 <= arr[i] <= 1000
+    1 <= k <= 1000
+    arr[i] < arr[j] for 1 <= i < j <= arr.length
+    '''
     def findKthPositive(self, arr: List[int], k: int) -> int:
+        i, j = 1, 0
+        while j < k:
+            if i not in arr:
+                num = i
+                j += 1
+            i += 1
+        return num
+#2195
